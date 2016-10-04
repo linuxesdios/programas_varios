4505c883092d20ee05ccdb5e26b2dd1ec02e148c
linuxesdios <linuxesdios@gmail.com>
2016-10-04 11:44:19 +0200

    Update conv.bat

--- conv.bat
+++ conv.bat
@@ -2,14 +2,19 @@
 :: convert proteus files gerber to made in factory(itead)
 
 set /p var=Nonbre del proyecto?: 
-ren *" - CADCAM Mechanical 1.TXT" "%var%".GKO
-ren *" - CADCAM Bottom Copper.TXT" "%var%".GBL
-ren *" - CADCAM Bottom Silk Screen.TXT" "%var%".GBO
-ren *" - CADCAM Bottom Solder Resist.TXT" "%var%".GBS
-ren *" - CADCAM Drill.TXT" "%var%".TXT
-ren *" - CADCAM Top Copper.TXT"  "%var%".GTL
-ren *" - CADCAM Top Silk Screen.TXT"  "%var%".GTO
-ren *" - CADCAM Top Solder Resist.TXT" "%var%".GTS
-del *" - CADCAM Bottom SMT Paste Mask.TXT"
+ren *" - CADCAM Mechanical 1.GBR" "%var%".GKO
+ren *" - CADCAM Bottom Copper.GBR" "%var%".GBL
+ren *" - CADCAM Bottom Silk Screen.GBR" "%var%".GBO
+ren *" - CADCAM Bottom Solder Resist.GBR" "%var%".GBS
+ren *" - CADCAM Drill L1 L16 Plated.GBR" "%var%".TXT
+ren *" - CADCAM Top Copper.GBR"  "%var%".GTL
+ren *" - CADCAM Top Silk Screen.GBR"  "%var%".GTO
+ren *" - CADCAM Top Solder Resist.GBR" "%var%".GTS
+del *" - CADCAM Bottom SMT Paste.GBR"
+del *" - CADCAM READ-ME.GBR"
+del *" - CADCAM Top SMT Paste.GBR"
+del *" - CADCAM Top SMT Paste.GBR"
+del *" - CADCAM Bottom Assembly.GBR"
+del *" - CADCAM Netlist.IPC"
 del *" - CADCAM READ-ME.TXT"
-del *" - CADCAM Top SMT Paste Mask.TXT"
+del *" - CADCAM Top Assembly.GBR"
